231fdc2de72f5c306f071bd134340b9d23a4c76b
ryeonthesulfur <perplexing2004@gmail.com>
2026-08-29 16:14:13 +0900

    ルーティングの編集
    ・子フォルダのタイトル変更（PATCH）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  resources :memos, only: [ :create, :show, :update ]
- resources :folders, only: [ :create, :show ]
+ resources :folders, only: [ :create, :show, :update ]
  root "top#index"
 end
