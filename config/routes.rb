690a05db263d31e8a05d316e7cc01564196d2b0e
ryeonthesulfur <perplexing2004@gmail.com>
2026-09-01 23:04:33 +0900

    ルーティングの編集
    ・削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources :memos, only: [ :create, :show, :update ]
- resources :folders, only: [ :create, :show, :update ]
+ resources :memos, only: [ :create, :show, :update, :destroy ]
+ resources :folders, only: [ :create, :show, :update, :destroy ]
  root "top#index"
 end
