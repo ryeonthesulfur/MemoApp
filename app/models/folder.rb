219bf9d3d1aedb901ec2e6bb0383b94f7e5b6b75
ryeonthesulfur <perplexing2004@gmail.com>
2026-09-14 18:23:40 +0900

    Folder モデルの編集
    ・複製のための関数定義
    ・メインパネル直下のメモ・フォルダ、また、その入れ子にあるものを複製する。

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,6 +2,46 @@ class Folder < ApplicationRecord
   belongs_to :parent, class_name: "Folder", optional: true
   has_many :children, class_name: "Folder", foreign_key: "parent_id", dependent: :destroy
   has_many :memos, dependent: :destroy # ▲▲▲ このフォルダの中にあるメモ一覧を取れるようにする＋親フォルダを消すと中身も消す。
+
+  # ▲▲▲ このフォルダを、中身(子フォルダ・メモ)ごと再帰的に複製する。複製先の親は元と同じ(parent_id)。
+  # 途中で失敗した場合に中途半端な複製が残らないよう、全体を1つのtransactionで囲む。
+  def deep_duplicate
+    ActiveRecord::Base.transaction do
+      duplicate_into(parent_id)
+    end
+  end
+
+  # 同じparent_idを持つ兄弟の中で、ベース名の"_copy(N)"のうち最大のNを探し、その+1を使った名前を返す
+  def self.next_copy_name(base_name, parent_id)
+    base = base_name.to_s.sub(/_copy\(\d+\)\z/, "")
+    max_n = where(parent_id: parent_id)
+              .where("name LIKE ?", "#{base}_copy(%")
+              .filter_map { |f| f.name[/_copy\((\d+)\)\z/, 1]&.to_i }
+              .max || 0
+    "#{base}_copy(#{max_n + 1})"
+  end
+
+  # 指定したparent_idの下に自分自身（大元のフォルダ）のコピーを1個作り、その後、中にmemos・childrenを再帰的に複製していく
+  # ▲▲▲ children.each { |child| child.duplicate_into(...) } のように、別のインスタンス(child)に対して
+  # 明示的に呼び出す必要があるため、private にはできない(Rubyのprivateは基本、自分自身への暗黙のselfでしか呼べない)
+  def duplicate_into(target_parent_id)
+    new_folder = dup
+    new_folder.parent_id = target_parent_id   # 最初は、大元のフォルダからの複製なので、nil。その後は、「大元のフォルダ_copy(N)」のidが入る。
+    new_folder.name = self.class.next_copy_name(name, target_parent_id)
+    new_folder.save!
+
+    # 大元のフォルダや、その中にある子フォルダの中のメモを、複製先の新しいフォルダにコピーする
+    memos.each do |memo|
+      new_memo = memo.dup
+      new_memo.folder_id = new_folder.id
+      new_memo.title = Memo.next_copy_name(memo.title, new_folder.id)
+      new_memo.save!
+    end
+
+    children.each { |child| child.duplicate_into(new_folder.id) }
+
+    new_folder
+  end
 end
 
 
